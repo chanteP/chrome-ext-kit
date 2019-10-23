0a4f59d39ffc204abfe31581d46eb412073096ac
chantep <541149506@qq.com>
2019-10-23 17:22:38 +0800

    修改打包文件

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 env=$1 
 [ -z $env ] && env='development'
 
-npx pack src/content/index.js -o dist/content.js --mode $env
 npx pack src/popup/index.js -o dist/popup.js --mode $env
+npx pack src/content/index.js -o dist/content.js --mode $env
 npx pack src/options.js -o dist/options.js --mode $env
 npx pack src/background.js -o dist/background.js --mode $env
 cp src/*.html ./dist
 
+rm -rf ./package/package/*
+mkdir -p ./package/package/
+if [ "$env" = 'production' ]; then
+
+  cp -r ./dist/ ./package/package/dist
+  cp -r ./lib/ ./package/package/lib
+  cp -r ./images/ ./package/package/images
+  cp manifest.json ./package/package/
+
+fi
